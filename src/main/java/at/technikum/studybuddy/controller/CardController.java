2810b8ebc32ecf81d8f2973e81419b42b9a02669
EA <100370471+evleh@users.noreply.github.com>
2025-05-28 11:54:58 +0200

    Beginn CardController

--- src/main/java/at/technikum/studybuddy/controller/CardController.java
+++ src/main/java/at/technikum/studybuddy/controller/CardController.java
@@ -1,4 +1,33 @@
 package at.technikum.studybuddy.controller;
 
+
+import at.technikum.studybuddy.entity.Card;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cards")
 public class CardController {
+
+    @GetMapping
+    public List<Card> getAll(){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Card get(@PathVariable int id){
+        return null;
+    }
+
+    @PostMapping
+    public Card create(Card card){
+        return null;
+    }
+
+    @PutMapping{"/{id}"}
+    public Card update(@PathVariable int id, @RequestBody Card card){
+        return null;
+    }
+
 }
--- src/main/java/at/technikum/studybuddy/entity/Card.java
+++ src/main/java/at/technikum/studybuddy/entity/Card.java
@@ -1,4 +1,73 @@
 package at.technikum.studybuddy.entity;
 
+import java.time.Instant;
+
 public class Card {
+    private int id;
+    // private int boxId;
+    private String question;
+    private String answer;
+    private String media;
+    private Instant created;
+    private Instant lastEdit;
+
+    public Card() {
+    }
+
+    public Card(int id, String question, String answer, String media, Instant created, Instant lastEdit) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.media = media;
+        this.created = created;
+        this.lastEdit = lastEdit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getLastEdit() {
+        return lastEdit;
+    }
+
+    public void setLastEdit(Instant lastEdit) {
+        this.lastEdit = lastEdit;
+    }
 }
