cc11fcfa2db23b9182ee28b0bf1cc39caffd5aeb
ww24e018 <ww24e018@technikum-wien.at>
2025-05-28 11:51:27 +0200

    Box and comment entities (#9)
    * middle commit for stashing
    
    * seems to work (box+boxcomment)

---
+++ src/main/java/at/technikum/studybuddy/repository/BoxRepository.java
@@ -0,0 +1,8 @@
+package at.technikum.studybuddy.repository;
+
+
+import at.technikum.studybuddy.entity.Box;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoxRepository extends JpaRepository<Box, Long> {
+}
