c2b095b4e637ff8591d4fbb7a17134d9f9b79cb2
ww24e018 <ww24e018@technikum-wien.at>
2025-06-02 14:41:02 +0200

    Boxcomments with dtos :: controller <-> db chain working (in principle) (#17)
    * boxcomment: dto exists, code compiles, swagger sees it.
    
    * undo experiment
    
    * posting boxcomments to database seems to work
    
    * getting works now
    
    * now working: get::id
    
    * deleting works

--- src/main/java/at/technikum/studybuddy/controller/BoxCommentController.java
+++ src/main/java/at/technikum/studybuddy/controller/BoxCommentController.java
@@ -1,5 +1,6 @@
 package at.technikum.studybuddy.controller;
 
+import at.technikum.studybuddy.dto.BoxCommentDto;
 import at.technikum.studybuddy.entity.BoxComment;
 import at.technikum.studybuddy.service.BoxCommentService;
 import jakarta.validation.Valid;
@@ -21,32 +22,29 @@ public class BoxCommentController {
 
     @GetMapping
     public List<BoxComment> getBoxComments() {
-        return null;
+        return this.boxCommentService.getAllBoxComments();
     }
 
     @GetMapping("/{id}")
     public BoxComment getBoxCommentById(@PathVariable Long id) {
-        return null;
+        return this.boxCommentService.getBoxCommentById(id);
     }
 
     @PostMapping
-    public BoxComment createBoxComment(@Valid @RequestBody BoxComment boxComment) {
-        return boxComment;
+    public BoxComment createBoxComment(@Valid @RequestBody BoxCommentDto boxCommentDto) {
+        // returning dto or entity itself? currently no reason wrap it that I can see.
+        return this.boxCommentService.createBoxComment(boxCommentDto);
     }
 
     @PutMapping("/{id}")
-    public BoxComment updateBoxComment(@PathVariable Long id, @Valid @RequestBody BoxComment boxComment) {
-        /* to be refactored to the service; but until service exists this is a workaround for a swagger behaviour */
-        if (boxComment.getId().equals(id) && id != 0) {
-            return boxComment;
-        } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ids in url and body params don't match.");
-        }
+    public BoxComment updateBoxComment(@PathVariable Long id, @Valid @RequestBody BoxCommentDto boxCommentDto) {
+        // if an id is in the dto/requestBody, the service-behaviour is "ignore"
+        return this.boxCommentService.updateBoxComment(id, boxCommentDto);
     }
 
     @DeleteMapping("/{id}")
-    public Long deleteBoxCommentById(@PathVariable Long id) {
-        return id;
+    public BoxComment deleteBoxCommentById(@PathVariable Long id) {
+        return this.boxCommentService.deleteBoxComment(id);
     }
 
 }
--- src/main/java/at/technikum/studybuddy/service/BoxCommentService.java
+++ src/main/java/at/technikum/studybuddy/service/BoxCommentService.java
@@ -1,7 +1,66 @@
 package at.technikum.studybuddy.service;
 
+import at.technikum.studybuddy.dto.BoxCommentDto;
+import at.technikum.studybuddy.entity.BoxComment;
+import at.technikum.studybuddy.exceptions.ResourceNotFoundException;
+import at.technikum.studybuddy.repository.BoxCommentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoxCommentService {
+
+    private final BoxCommentRepository boxCommentRepository;
+    public BoxCommentService(BoxCommentRepository boxCommentRepository) {
+        this.boxCommentRepository = boxCommentRepository;
+    }
+
+    public BoxComment createBoxComment(BoxCommentDto boxCommentDto) {
+        BoxComment boxComment = new BoxComment();
+        boxComment.setText(boxCommentDto.getText());
+        return boxCommentRepository.save(boxComment);
+    }
+
+    public List<BoxComment> getAllBoxComments() {
+        return this.boxCommentRepository.findAll();
+    }
+
+    public BoxComment getBoxCommentById(Long id) {
+        return this.boxCommentRepository.findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+        /* note:
+          ResourceNotFoundException::new
+          is an ide-suggest replacement for () -> new ResourceNotFoundException
+          which is shorthand for if(..isempty()) ... etc
+        */
+    }
+
+    public BoxComment updateBoxComment(Long id, BoxCommentDto boxCommentDto) {
+        BoxComment boxComment = getBoxCommentById(id);
+        boxComment.setText(boxCommentDto.getText());
+        return boxCommentRepository.save(boxComment);
+    }
+
+    public BoxComment deleteBoxComment(Long id) {
+        Optional<BoxComment> boxComment = this.boxCommentRepository.findById(id);
+        boxComment.orElseThrow(ResourceNotFoundException::new);
+        // comment for line above: orElseThrow does not return an optional, so no chaining :(
+
+        try {
+            // ide suggests; suspect makes it easier to change pks later?
+            // docs seem to say delete will look up with Ids from the object here
+            boxCommentRepository.delete(boxComment.get());
+        } catch (IllegalArgumentException e) {
+            // in case it got deleted by someone else in between?
+            // might be too much, idk.
+            throw new ResourceNotFoundException();
+        }
+        // assuming everything went smooth, then:
+        return boxComment.get();
+
+    }
+
+
 }
