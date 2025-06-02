9ff0526b4137ce04cd14a70806fcc722cc49c33e
soschoen <sophie.schoenwaelder.rieder@gmail.com>
2025-06-02 15:42:39 +0200

    subscription

--- src/main/java/at/technikum/studybuddy/entity/Subscription.java
+++ src/main/java/at/technikum/studybuddy/entity/Subscription.java
@@ -29,8 +29,36 @@ public class Subscription {
 
     //full constructor
 
-    public Subscription(long id, Instant createdAt) {
+    public Subscription(long id, Instant createdAt, Instant updatedAt) {
         this.id = id;
         this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    //getters
+    public long getId() {
+        return id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    //Setters
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
