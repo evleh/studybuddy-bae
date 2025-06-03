c6f68c578b33b6763c0b073521f8cfc7e8856c9c
ww24e018 <ww24e018@technikum-wien.at>
2025-06-03 09:25:18 +0200

    cleanup: removed unused import in BoxCommentController

--- src/main/java/at/technikum/studybuddy/controller/BoxCommentController.java
+++ src/main/java/at/technikum/studybuddy/controller/BoxCommentController.java
@@ -6,7 +6,6 @@ import at.technikum.studybuddy.service.BoxCommentService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
