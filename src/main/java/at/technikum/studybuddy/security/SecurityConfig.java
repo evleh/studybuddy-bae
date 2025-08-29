caebb7e8a806c73fdb709dfa015893d62195bd89
ww24e018 <ww24e018@technikum-wien.at>
2025-08-29 12:09:22 +0200

    not exact last lecture state methinks.

--- src/main/java/at/technikum/studybuddy/security/SecurityConfig.java
+++ src/main/java/at/technikum/studybuddy/security/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.rmi.registry.Registry;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +17,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http.cors().and().csrf().disable();
+
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         http
                 .formLogin()
                 .disable();
+
+        http.
+                authorizeHttpRequests(
+                        registry -> registry
+                                .requestMatchers("/auth/register").permitAll()
+                                .requestMatchers("/auth/login").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/api/**").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
 
         return http.build();
     }
