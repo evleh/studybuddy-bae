25ce9de3a15740db92505110e701570be3ad8f46
ww24e018 <ww24e018@technikum-wien.at>
2025-09-19 12:07:06 +0200

    merge Lecturecode/2025 09 05 (minus Service/Controller Adaptions) (#26)
    * porting lecture code.
    
    currently compiles. this seems to be the "security" "package" done, with changes here and there as required.
    
     this commits motto: "imports will continue until morale improves"
    
    * more porting: the dto folder;
    
    on itself does nothing ofc. therefore unsurprisingly it compiles (and still works on basic testing).
    
    * more porting: config/swaggerconfig
    
    seems to enable the "lock" icons in the swagger ui; compact
    
    still compiles and seems to work
    
    * more porting lecture code: it defines these 2 exceptions for later use.
    
    can't tell if verbosity of declaration strictly necessary. but possibly not dangerous.
    
    still compiles and seems to work

---
+++ src/main/java/at/technikum/studybuddy/dto/Token.java
@@ -0,0 +1,14 @@
+package at.technikum.studybuddy.dto;
+
+public class Token {
+
+    private String accessToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
---
+++ src/main/java/at/technikum/studybuddy/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package at.technikum.studybuddy.security;
+
+import at.technikum.studybuddy.entity.User;
+import at.technikum.studybuddy.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new UserPrincipal(
+                Long.toString(user.getId()),
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
