7ecffedbfc223d3699d286bc10e3263bf07ed126
ww24e018 <ww24e018@technikum-wien.at>
2025-10-24 15:35:06 +0200

    add jwt mechanism with guidance by altin
    change is transparent to frontend; not production ready yet, but proof-of-codepathes-to-walk

--- src/main/java/at/technikum/studybuddy/service/AuthService.java
+++ src/main/java/at/technikum/studybuddy/service/AuthService.java
@@ -3,11 +3,23 @@ package at.technikum.studybuddy.service;
 import at.technikum.studybuddy.dto.Token;
 import at.technikum.studybuddy.dto.TokenRequest;
 import at.technikum.studybuddy.security.UserPrincipal;
+import at.technikum.studybuddy.security.UserPrincipalAuthenticationToken;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class AuthService {
@@ -30,9 +42,20 @@ public class AuthService {
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
 
         // real JWT here
+        String jwt = JWT.create()
+                .withExpiresAt(Instant.now().plus(20, ChronoUnit.DAYS))
+                .withClaim("userId", userPrincipal.getUserId())
+                .withIssuer("studybuddy-bae")
+                .withIssuedAt(Date.from(Instant.now()))
+                .sign(Algorithm.HMAC256("le secret"))
+                ;
+
+
         Token token = new Token();
-        token.setAccessToken(userPrincipal.getUserId());
+        token.setAccessToken(jwt);
         token.setUserId(userPrincipal.getUserId());
         return token;
     }
+
+
 }
