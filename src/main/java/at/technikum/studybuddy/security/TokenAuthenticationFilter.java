f552de49021c6e852e55effb3d228df3d5ba0294
ww24e018 <ww24e018@technikum-wien.at>
2025-10-24 14:18:41 +0200

    works

--- src/main/java/at/technikum/studybuddy/security/TokenAuthenticationFilter.java
+++ src/main/java/at/technikum/studybuddy/security/TokenAuthenticationFilter.java
@@ -4,6 +4,12 @@
 
 package at.technikum.studybuddy.security;
 
+import at.technikum.studybuddy.service.AuthService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +25,7 @@ import java.util.List;
 
 @Component
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
 
     @Override
     protected void doFilterInternal(
@@ -36,9 +43,35 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
-        String userId = request
+        this.verifyToken(request
                 .getHeader("Authorization")
-                .replace("Bearer ", "");
+                .replace("Bearer ", "")
+        );
+
+
+        filterChain.doFilter(request, response);
+    }
+    private void verifyToken(String jwt) {
+        String userId = null;
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256("le secret"))
+                    // specify any specific claim validations
+                    .withIssuer("studybuddy-bae")
+                    // reusable verifier instance
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(jwt);
+            userId = decodedJWT.getClaim("userId").asString();
+
+        } catch (JWTVerificationException exception){
+            // Invalid signature/claims
+            throw new RuntimeException(exception.getMessage());
+        }
+
+        /*String userId = request
+                .getHeader("Authorization")
+                .replace("Bearer ", "");*/
+
         Authentication authentication = new UserPrincipalAuthenticationToken(
                 new UserPrincipal(
                         userId,
@@ -48,8 +81,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
                 )
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
-
-        filterChain.doFilter(request, response);
+        // hashTag findMeAgain #findmeagain also TODO
     }
+
 }
