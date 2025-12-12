8e0a1fd762ad28154b995e802c3a78c79049f2c8
ww24e018 <ww24e018@technikum-wien.at>
2025-12-12 14:42:04 +0100

    code review

--- src/main/java/at/technikum/studybuddy/security/TokenAuthenticationFilter.java
+++ src/main/java/at/technikum/studybuddy/security/TokenAuthenticationFilter.java
@@ -15,7 +15,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -29,8 +28,11 @@ import java.util.List;
 @Component
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public TokenAuthenticationFilter(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     protected void doFilterInternal(
