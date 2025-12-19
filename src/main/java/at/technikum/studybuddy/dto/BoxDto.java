d833b7bee01f5925587e2d74f7973e5635a46d0d
EA <100370471+evleh@users.noreply.github.com>
2025-12-19 11:03:00 +0100

    Review change requests:  remove comment

--- src/main/java/at/technikum/studybuddy/dto/BoxDto.java
+++ src/main/java/at/technikum/studybuddy/dto/BoxDto.java
@@ -19,7 +19,6 @@ public class BoxDto {
 
     private Boolean isPublic;
 
-    // Entity connections. Currently, DTOs return only return ids. one todo is to make this more realistic.
     private Long ownerId;
     private List<Long> commentIds;
     private List<Long> cardIds;
