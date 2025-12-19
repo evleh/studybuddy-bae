c4a8520fd532979a4711a5801f880d8ed4a54b41
ww24e018 <ww24e018@technikum-wien.at>
2025-12-19 13:55:08 +0100

    Revert "savepoint for disentangle"
    This reverts commit 13eafdfdd9b479276652780a3f7cab3ab59ddec0.

--- src/main/java/at/technikum/studybuddy/controller/AuthController.java
+++ src/main/java/at/technikum/studybuddy/controller/AuthController.java
@@ -4,8 +4,6 @@ import at.technikum.studybuddy.dto.Token;
 import at.technikum.studybuddy.dto.TokenRequest;
 import at.technikum.studybuddy.service.AuthService;
 import jakarta.validation.Valid;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -21,11 +19,6 @@ public class AuthController {
 
     @PostMapping("/token")
     public Token token(@RequestBody @Valid TokenRequest tokenRequest) {
-        Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
-        boolean x = "anonymousUser".equals(SecurityContextHolder.getContext().getAuthentication().toString());
-        System.err.format("%s\n", SecurityContextHolder.getContext().getAuthentication().toString());
-        System.err.format("%s\n", SecurityContextHolder.getContext().getAuthentication().getClass());
-        System.err.println(org.springframework.security.authentication.AnonymousAuthenticationToken.class);
         return authService.createToken(tokenRequest);
     }
 }
