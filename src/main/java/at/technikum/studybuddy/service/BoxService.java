8ec8bd6847958de0797210aab72aa4813e6ea9bd
EA <100370471+evleh@users.noreply.github.com>
2025-12-29 17:37:22 +0100

    Refactor crud-methods to match naming pattern in other classes

--- src/main/java/at/technikum/studybuddy/service/BoxService.java
+++ src/main/java/at/technikum/studybuddy/service/BoxService.java
@@ -6,6 +6,8 @@ import at.technikum.studybuddy.entity.User;
 import at.technikum.studybuddy.exceptions.ResourceNotFoundException;
 import at.technikum.studybuddy.repository.BoxRepository;
 import at.technikum.studybuddy.repository.UserRepository;
+import at.technikum.studybuddy.security.RoleTypes;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -25,15 +27,16 @@ public class BoxService {
         this.userService = userService;
     }
 
-    public List<Box> readAllBoxes() {
+    @RolesAllowed(RoleTypes.ADMIN)
+    public List<Box> readAll() {
         return boxRepository.findAll();
     }
 
-    public Box readBoxById(Long id) {
+    public Box read(Long id) {
         return boxRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
     }
 
-    public Box createBox(BoxDto boxDto) {
+    public Box create(BoxDto boxDto) {
         // todo remove ownerId from boxDto??
         Optional<User> owner = this.userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
         if(owner.isEmpty()){
@@ -43,13 +46,13 @@ public class BoxService {
         return boxRepository.save(box);
     }
 
-    public Box updateBox(Long id, BoxDto boxDto) {
+    public Box update(Long id, BoxDto boxDto) {
         Box box = boxRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
         return boxRepository.save(box.updateFromBoxDto(boxDto));
     }
 
     // change to return dto, to (hopefully) avoid the org.hibernate.LazyInitializationException
-    public BoxDto deleteBox(Long id) {
+    public BoxDto delete(Long id) {
         Box box = boxRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
         BoxDto boxDto = new BoxDto(box);
         boxRepository.delete(box);
