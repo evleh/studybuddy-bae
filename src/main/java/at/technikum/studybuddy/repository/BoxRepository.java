edce3e1257f3796769ea433d8b6f23591216f383
ww24e018 <ww24e018@technikum-wien.at>
2026-01-16 12:07:03 +0100

    add: dto/repointerface/controller/serivce methods for public boxes also security exceptions; worked in simple swagger testing

--- src/main/java/at/technikum/studybuddy/repository/BoxRepository.java
+++ src/main/java/at/technikum/studybuddy/repository/BoxRepository.java
@@ -4,5 +4,8 @@ package at.technikum.studybuddy.repository;
 import at.technikum.studybuddy.entity.Box;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoxRepository extends JpaRepository<Box, Long> {
+    List<Box>findBoxesByIsPublicIsTrue();
 }
