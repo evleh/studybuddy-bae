a6196b60a1ff6bd1fb9277ce6369fb907f1b3f9e
ww24e018 <ww24e018@technikum-wien.at>
2026-02-09 13:00:31 +0100

    add /me/self endpoint (rfc)

--- src/main/java/at/technikum/studybuddy/controller/MeController.java
+++ src/main/java/at/technikum/studybuddy/controller/MeController.java
@@ -1,8 +1,10 @@
 package at.technikum.studybuddy.controller;
 
 import at.technikum.studybuddy.dto.BoxDto;
+import at.technikum.studybuddy.dto.UserDtoPrivilegedInfo;
 import at.technikum.studybuddy.security.UserPrincipal;
 import at.technikum.studybuddy.service.BoxService;
+import at.technikum.studybuddy.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,9 +19,11 @@ import java.util.List;
 @RequestMapping("/me")
 public class MeController {
     private final BoxService boxService;
+    private final UserService userService;
 
-    public MeController(BoxService boxService) {
+    public MeController(BoxService boxService, UserService userService) {
         this.boxService = boxService;
+        this.userService = userService;
     }
 
     private Long readUserIdFromSecurityContext() {
@@ -31,6 +35,13 @@ public class MeController {
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_REGISTERED')")
     public List<BoxDto> readOwnBoxes() {
         return this.boxService.readBoxesOfUser(this.readUserIdFromSecurityContext()).stream().map(BoxDto::new).toList();
+    }
+
+    @GetMapping("/self")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_REGISTERED')")
+    public UserDtoPrivilegedInfo readOwnUserInfo() {
+        var userInfo = this.userService.read(this.readUserIdFromSecurityContext());;
+        return new UserDtoPrivilegedInfo(userInfo);
     }
 
 }
