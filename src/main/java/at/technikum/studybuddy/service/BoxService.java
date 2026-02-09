332cd1b6f8a92c61e700aa9fba8ffb11ca529643
EA <100370471+evleh@users.noreply.github.com>
2026-02-09 11:18:32 +0100

    Removed unused dependencies

--- src/main/java/at/technikum/studybuddy/service/BoxService.java
+++ src/main/java/at/technikum/studybuddy/service/BoxService.java
@@ -8,8 +8,6 @@ import at.technikum.studybuddy.exceptions.ResourceNotFoundException;
 import at.technikum.studybuddy.repository.BoxRepository;
 import at.technikum.studybuddy.repository.UserRepository;
 import at.technikum.studybuddy.security.UserPrincipal;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
