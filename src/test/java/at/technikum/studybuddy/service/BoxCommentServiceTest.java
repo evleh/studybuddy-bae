c6995e2a2ccb903612950379d9181fe363b84293
EA <100370471+evleh@users.noreply.github.com>
2026-02-13 12:01:29 +0100

    Initialize testclass and write first test.

---
+++ src/test/java/at/technikum/studybuddy/service/BoxCommentServiceTest.java
@@ -0,0 +1,45 @@
+package at.technikum.studybuddy.service;
+
+import at.technikum.studybuddy.entity.BoxComment;
+import at.technikum.studybuddy.repository.BoxCommentRepository;
+import at.technikum.studybuddy.repository.BoxRepository;
+import at.technikum.studybuddy.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@ExtendWith(MockitoExtension.class)
+public class BoxCommentServiceTest {
+
+    @Mock
+    private BoxCommentRepository boxCommentRepository;
+
+    @Mock
+    private BoxRepository boxRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private BoxCommentService boxCommentService;
+
+    @Test
+    void readAllOnEmptyRepository(){
+        Mockito.when(boxCommentRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<BoxComment> comments = boxCommentService.readAll();
+
+        assertTrue(comments.isEmpty());
+    }
+}
